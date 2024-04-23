b422c054d945ef10c3a8a6abd617029b1d974698
Arthur Gonçalves <112991508+Sheiden1@users.noreply.github.com>
2024-04-23 16:21:22 -0300

    Update script.js

--- app/static/script.js
+++ app/static/script.js
@@ -1,56 +1,65 @@
-const grids = document.querySelectorAll('.grid')
-const headings = document.querySelectorAll('.heading .wrapper .text')
+const grids = document.querySelectorAll('.grid');
+const headings = document.querySelectorAll('.heading .wrapper .text');
+const images = ['/eu.jpeg', '/suga.png', '/az.jpg'];
+let currentImageIndex = 0;
 
 function enterScreen(index) {
-  const grid = grids[index]
-  const heading = headings[index]
-  const gridColumns = grid.querySelectorAll('.column')
+  const grid = grids[index];
+  const heading = headings[index];
+  const gridColumns = grid.querySelectorAll('.column');
 
-  grid.classList.add('active')
+  grid.classList.add('active');
 
-  gridColumns.forEach(element => {
-    element.classList.remove('animate-before', 'animate-after')
-  })
+  gridColumns.forEach((element) => {
+    element.classList.remove('animate-before', 'animate-after');
+  });
 
-  heading.classList.remove('animate-before', 'animate-after')
+  heading.classList.remove('animate-before', 'animate-after');
 }
 
 function exitScreen(index, exitDelay) {
-  const grid = grids[index]
-  const heading = headings[index]
-  const gridColumns = grid.querySelectorAll('.column')
+  const grid = grids[index];
+  const heading = headings[index];
+  const gridColumns = grid.querySelectorAll('.column');
 
-  gridColumns.forEach(element => {
-    element.classList.add('animate-after')
-  })
+  gridColumns.forEach((element) => {
+    element.classList.add('animate-after');
+  });
 
-  heading.classList.add('animate-after')
+  heading.classList.add('animate-after');
 
   setTimeout(() => {
-    grid.classList.remove('active')
-  }, exitDelay)
+    grid.classList.remove('active');
+  }, exitDelay);
 }
 
 function setupAnimationCycle({ timePerScreen, exitDelay }) {
-  const cycleTime = timePerScreen + exitDelay
-  let nextIndex = 0
+  const cycleTime = timePerScreen + exitDelay;
+  let nextIndex = 0;
 
   function nextCycle() {
-    const currentIndex = nextIndex
+    const currentIndex = nextIndex;
+    const currentImage = images[currentImageIndex];
+    const items = grids[currentIndex].querySelectorAll('.item');
 
-    enterScreen(currentIndex)
+    items.forEach((item) => {
+      item.style.backgroundImage = `url(${currentImage})`;
+    });
 
-    setTimeout(() => exitScreen(currentIndex, exitDelay), timePerScreen)
+    enterScreen(currentIndex);
 
-    nextIndex = nextIndex >= grids.length - 1 ? 0 : nextIndex + 1
+    setTimeout(() => exitScreen(currentIndex, exitDelay), timePerScreen);
+
+    nextIndex = nextIndex >= grids.length - 1 ? 0 : nextIndex + 1;
+    currentImageIndex = (currentImageIndex + 1) % images.length;
   }
 
-  nextCycle()
+  nextCycle();
 
-  setInterval(nextCycle, cycleTime)
+  setInterval(nextCycle, cycleTime);
 }
 
 setupAnimationCycle({
   timePerScreen: 2000, // ms
-  exitDelay: 200 * 7 // ms
-})
+  exitDelay: 200 * 7, // ms
+});
