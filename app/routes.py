62bfd5b289aff96e9a36f9e6578d89e41d91673e
rphldbm <rphlldbm@gmail.com>
2024-05-23 12:39:42 -0300

    atualizaçoes

--- app/routes.py
+++ app/routes.py
@@ -6,5 +6,9 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run()
