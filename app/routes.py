2127ded397eae5af7b3311e2d89fc59c3bb0cefc
Raphael <rphlldbm@gmail.com>
2024-05-28 17:58:15 -0300

    maquina nova

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run()
